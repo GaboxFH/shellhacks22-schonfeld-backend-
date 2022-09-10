58e27f40baab91d657766c3b3212b76d4b6b913f
Gabe <gabefernandez@ufl.edu>
2022-09-10 13:24:43 -0400

    Added .env file to handle credentials more securely

--- app/database.py
+++ app/database.py
@@ -1,11 +1,21 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import os
+
+from dotenv import load_dotenv
+load_dotenv()  # take environment variables from .env.
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+print("This is a test")
+print(DATABASE_URL)
 
 # SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
 # SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
 # postgresql://admin@name:password@name.postgres.database.azure.com:5432/postgres
-SQLALCHEMY_DATABASE_URL = "postgresql://securiquery:sh3llHacks@securiquery.postgres.database.azure.com:5432/postgres"
+# SQLALCHEMY_DATABASE_URL = "postgresql://securiquery:sh3llHacks@securiquery.postgres.database.azure.com:5432/postgres"
+SQLALCHEMY_DATABASE_URL = DATABASE_URL
 
 
 engine = create_engine(
